1d200eebd3f57a95916e8aaef45ec337dc93a7dd
Arun Agrawal <arunagw@gmail.com>
2015-10-09 09:50:28 +0200

    Do not deactivate repo on update

--- app/services/update_repo_status.rb
+++ app/services/update_repo_status.rb
@@ -4,7 +4,6 @@ class UpdateRepoStatus
   def run
     if repo
       repo.update_attributes(repo_attributes)
-      deactive_repo(repo)
     end
   end
 
@@ -19,16 +18,5 @@ class UpdateRepoStatus
       full_github_name: payload.full_repo_name,
       private: payload.private_repo?,
     }
-  end
-
-  def deactive_repo(repo)
-    if repo.previous_changes[:private] == change_public_to_private
-      repo.deactivate
-    end
-  end
-
-  def change_public_to_private
-    # Active Record Dirty objects keep track after save
-    [false, true]
   end
 end
--- spec/services/update_repo_status_spec.rb
+++ spec/services/update_repo_status_spec.rb
@@ -2,108 +2,36 @@ require "rails_helper"
 
 describe UpdateRepoStatus do
   describe "#run" do
-    context "For an active repo" do
-      it "updates the repo full_github_name" do
-        expected_repo_name = "foo/bar"
-        repo = create(:repo, :active)
-        payload = double(
-          "Payload",
-          github_repo_id: repo.github_id,
-          full_repo_name: expected_repo_name,
-          private_repo?: repo.private,
-        )
+    it "updates the repo full_github_name" do
+      expected_repo_name = "foo/bar"
+      repo = create(:repo, :active)
+      payload = double(
+        "Payload",
+        github_repo_id: repo.github_id,
+        full_repo_name: expected_repo_name,
+        private_repo?: repo.private,
+      )
 
-        UpdateRepoStatus.new(payload).run
+      UpdateRepoStatus.new(payload).run
 
-        repo.reload
-        expect(repo.full_github_name).to eq(expected_repo_name)
-      end
-
-      it "updates the private flag for repo" do
-        expected_status = true
-        repo = create(:repo, :active)
-        payload = double(
-          "Payload",
-          github_repo_id: repo.github_id,
-          full_repo_name: repo.full_github_name,
-          private_repo?: expected_status,
-        )
-
-        UpdateRepoStatus.new(payload).run
-
-        repo.reload
-        expect(repo.private).to eq(expected_status)
-      end
-
-      context "when repo become private from public" do
-        it "deactivates repo" do
-          repo = create(:repo, :active, private: false)
-          payload = double(
-            "Payload",
-            github_repo_id: repo.github_id,
-            full_repo_name: repo.full_github_name,
-            private_repo?: true,
-          )
-
-          UpdateRepoStatus.new(payload).run
-
-          repo.reload
-          expect(repo).not_to be_active
-        end
-      end
-
-      context "when repo become public from private" do
-        it "will do nothing" do
-          repo = create(:repo, :active, private: true)
-          payload = double(
-            "Payload",
-            github_repo_id: repo.github_id,
-            full_repo_name: repo.full_github_name,
-            private_repo?: false,
-          )
-
-          UpdateRepoStatus.new(payload).run
-
-          repo.reload
-          expect(repo).to be_active
-        end
-      end
+      repo.reload
+      expect(repo.full_github_name).to eq(expected_repo_name)
     end
 
-    context "for a inactive repo" do
-      context "when repo become private from public" do
-        it "will do nothing" do
-          repo = create(:repo, :inactive, private: true)
-          payload = double(
-            "Payload",
-            github_repo_id: repo.github_id,
-            full_repo_name: repo.full_github_name,
-            private_repo?: true,
-          )
+    it "updates the private flag for repo" do
+      expected_status = true
+      repo = create(:repo, :active)
+      payload = double(
+        "Payload",
+        github_repo_id: repo.github_id,
+        full_repo_name: repo.full_github_name,
+        private_repo?: expected_status,
+      )
 
-          UpdateRepoStatus.new(payload).run
+      UpdateRepoStatus.new(payload).run
 
-          repo.reload
-          expect(repo).not_to be_active
-        end
-      end
-
-      context "when repo become public from private" do
-        it "will do nothing" do
-          repo = create(:repo, :inactive, private: false)
-          payload = double(
-            "Payload",
-            github_repo_id: repo.github_id,
-            full_repo_name: repo.full_github_name,
-            private_repo?: true,
-          )
-
-          UpdateRepoStatus.new(payload).run
-
-          repo.reload
-          expect(repo).not_to be_active
-        end
-      end
+      repo.reload
+      expect(repo.private).to eq(expected_status)
     end
   end
 end
